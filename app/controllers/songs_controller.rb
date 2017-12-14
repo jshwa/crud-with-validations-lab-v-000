a7d00441aba50a58ff7bf812211c237728898248
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-14 03:59:05 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,18 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @songs = Song.all
+  end
+
+  private
+
+  def song_params
+    params.require(@song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
 
 end
