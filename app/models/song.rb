ca2ea1ab753ed29054731c2c2a4f2d8554aa0b8b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-14 04:28:37 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, :artist_name, presence: true
   validates :released, inclusion: {in: [true, false]}
-  validates :release_year, inclusion: {in: [1...Time.now.year]}
+  validates :release_year, inclusion: {in: [1...Time.now.year]}, if: :released?
   validates :release_year, presence: true, if: :released?
   validate :title_year_check
 
