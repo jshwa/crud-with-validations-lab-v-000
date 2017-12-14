2906050672757341d3f68081aa4e476a79c077ab
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-14 04:05:35 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
   validates :title, presence: :true
   validates :released, presence: :true
-  validatse :artist_name, presence: :true
+  validates :artist_name, presence: :true
 end
